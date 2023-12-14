d63cacbe1ecfe4fe23b17abb5dc38ea7c8baf1be
thabassums <152673533+thabassums@users.noreply.github.com>
2023-12-14 10:43:01 +0530

    Add files via upload

---
+++ StartUp-Solutions-master/login.java
@@ -0,0 +1,16 @@
+# This is a sample java login page
+
+public class QuotientRemainder {
+
+  public static void main(String[] args) {
+
+    int dividend = 50, divisor = 40;
+# these changes are particular to Facebook login
+
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+
+    System.out.println("Quotient = " + quotient);
+    System.out.println("Remainder = " + remainder);
+  }
+}
